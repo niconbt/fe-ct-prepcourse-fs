fcaa4b38a615beba326d885571eba1f4b8f9c146
nicolasb <nicolas.benjamin.t@outlook.com>
2024-02-24 16:32:52 -0300

    update homework js

--- C5 Fundamentos Javascript/ejercicios/12.js
+++ C5 Fundamentos Javascript/ejercicios/12.js
@@ -2,6 +2,14 @@ function devolverString(string) {
   // La función recibe un argumento "string".
   // Debe retornar dicho string.
   // Tu código:
+  return string;
 }
+
+function nombre(){
+  var texto = "Nicolas";
+  var resultado = devolverString(texto);
+  console.log(resultado);
+}
+nombre();
 
 module.exports = devolverString;
