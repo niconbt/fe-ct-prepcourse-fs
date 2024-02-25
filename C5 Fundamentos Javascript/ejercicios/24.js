32db82a5e4ae3696e18218ac0eb2d1eb0a3de1ac
nicolasb <nicolas.benjamin.t@outlook.com>
2024-02-25 20:26:20 -0300

    Update Homework js

--- C5 Fundamentos Javascript/ejercicios/24.js
+++ C5 Fundamentos Javascript/ejercicios/24.js
@@ -2,8 +2,7 @@ function divide(x, y) {
   // La función recibe dos argumentos "x" e "y" que son números.
   // Retorna el resultado de su división.
   // Tu código:
-  var resultado = (x / y);
-  return resultado;
+  return (x / y);
 }
 
 module.exports = divide;
