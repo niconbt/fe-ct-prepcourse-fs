2da00963990eabc79f31cd528564336fc6b39070
nicolasb <nicolas.benjamin.t@outlook.com>
2024-02-25 14:28:11 -0300

    test

--- C5 Fundamentos Javascript/ejercicios/38.js
+++ C5 Fundamentos Javascript/ejercicios/38.js
@@ -1,9 +1,6 @@
 // ⛔️ Recuerda que debes utilizar el objeto global "Math".
-
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba y retórnalo.
   // Tu código:
-var num = Math.ceil(num);
-return num; 
 }
 module.exports = redondearHaciaArriba;
