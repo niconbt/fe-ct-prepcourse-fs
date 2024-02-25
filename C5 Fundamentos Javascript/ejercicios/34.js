2eb9aafcd7dba20ddd4dc6d0a3f6adb8c55b5377
nicolasb <nicolas.benjamin.t@outlook.com>
2024-02-25 14:36:48 -0300

    Update Homework js

--- C5 Fundamentos Javascript/ejercicios/34.js
+++ C5 Fundamentos Javascript/ejercicios/34.js
@@ -3,6 +3,8 @@
 function elevarAlCuadrado(num) {
   // Retorna el valor de "num" elevado al cuadrado.
   // Tu código:
+  var num = Math.pow(num, 2);
+  return num;
 }
 
 module.exports = elevarAlCuadrado;
