dd7cf4ef466a16e16c5f9d03aea0f1b1198eab6b
nicolasb <nicolas.benjamin.t@outlook.com>
2024-02-25 14:20:47 -0300

    Update Homework js

--- C5 Fundamentos Javascript/ejercicios/38.js
+++ C5 Fundamentos Javascript/ejercicios/38.js
@@ -3,6 +3,8 @@
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba y retórnalo.
   // Tu código:
-}
 
+var num = Math.ceil(num);
+return num; 
+}
 module.exports = redondearHaciaArriba;
