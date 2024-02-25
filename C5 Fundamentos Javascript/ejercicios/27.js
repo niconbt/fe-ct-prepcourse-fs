dbb247c8dfb38e48766dd584c827132b07bd8687
nicolasb <nicolas.benjamin.t@outlook.com>
2024-02-25 17:10:24 -0300

    Update Homework js

--- C5 Fundamentos Javascript/ejercicios/27.js
+++ C5 Fundamentos Javascript/ejercicios/27.js
@@ -2,8 +2,7 @@ function combinarNombres(nombre, apellido) {
   // Retorna "nombre" y "apellido" combinados en un mismo string pero separados por un espacio.
   // Ejemplo: ("Soy", "Henry") ---> "Soy Henry"
   // Tu código:
-  var texto = nombre  + " " + apellido ;
-  return texto; 
+  return nombre  + " " + apellido ;
 }
 
 module.exports = combinarNombres;
