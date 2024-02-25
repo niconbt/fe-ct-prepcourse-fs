af838389fb4a1192b3247d92c5815bd13a4ce746
nicolasb <nicolas.benjamin.t@outlook.com>
2024-02-25 20:23:29 -0300

    Update Homework js

--- C5 Fundamentos Javascript/ejercicios/26.js
+++ C5 Fundamentos Javascript/ejercicios/26.js
@@ -2,7 +2,6 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final del string "str" y retórnalo.
   // Ejemplo: "hello world" ---> "hello world!"
   // Tu código:
-  var exclamación = "!"
   return str + "!"; 
 }
 
