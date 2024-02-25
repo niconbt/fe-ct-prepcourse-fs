437288cb927b6a828f3efba8ab397a6847a09aef
nicolasb <nicolas.benjamin.t@outlook.com>
2024-02-25 20:25:06 -0300

    Update Homework js

--- C5 Fundamentos Javascript/ejercicios/25.js
+++ C5 Fundamentos Javascript/ejercicios/25.js
@@ -2,8 +2,7 @@ function obtenerResto(x, y) {
   // La función recibe dos argumentos "x" e "y" que son números.
   // Obten el resto de la división de "x" entre "y".
   // Tu código:
-  var resultado = (x % y);
-  return resultado;
-}
+ return  (x % y);
+  }
 
 module.exports = obtenerResto;
