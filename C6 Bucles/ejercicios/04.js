33abbfdd8d2fd21bef6c6bcc0239b84093f2b6a4
nicolasb <nicolas.benjamin.t@outlook.com>
2024-02-27 15:22:31 -0300

    Update Homework js bucles

--- C6 Bucles/ejercicios/04.js
+++ C6 Bucles/ejercicios/04.js
@@ -3,6 +3,12 @@ function mayoriaDeEdad(edad) {
   // Si tiene 18 años o más, retorna el string: "Allowed".
   // Caso contrario: "Not allowed".
   // Tu código:
+  for (let i = 0; i >=18; i++) {
+    if ( i >= 18){
+      return "Allowed";
+    } else if ( i < 18){
+      return "Not Allowed";
+    }
+  }
 }
-
 module.exports = mayoriaDeEdad;
