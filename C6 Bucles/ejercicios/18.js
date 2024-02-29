3d7094f507a9874751901116710a3fe71ade0371
nicolasb <nicolas.benjamin.t@outlook.com>
2024-02-28 22:50:30 -0300

    Update Homework js bucles

--- C6 Bucles/ejercicios/18.js
+++ C6 Bucles/ejercicios/18.js
@@ -2,6 +2,15 @@ function productoEntreNúmeros(a, b) {
   // Dados dos argumentos "a" y "b", devuelve el producto de todos
   // los números entre a y b (inclusive).
   // Tu código:
+  let contador = 1
+  for (let i = a; i <= b; i++){
+     contador = contador * i ;
+   
+  }
+  if (contador === -0){
+    return 0;
+  }
+  return contador;
 }
 
 module.exports = productoEntreNúmeros;
