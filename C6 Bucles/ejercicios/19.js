f46ea4fe7be8e987c7a4cc3377ba72b5c83231da
nicolasb <nicolas.benjamin.t@outlook.com>
2024-02-28 23:01:22 -0300

    Update Homework js bucles

--- C6 Bucles/ejercicios/19.js
+++ C6 Bucles/ejercicios/19.js
@@ -2,6 +2,12 @@ function sumarHastaN(n) {
   // La función recibe un número "n" por argumento.
   // Devuelve la suma de todos los números desde 1 hasta n.
   // Tu código:
+  let contador = 0;
+  for (let i = 1; i <= n; i++) {
+    contador = contador + i;
+    
+  }
+  return contador;
 }
 
 module.exports = sumarHastaN;
