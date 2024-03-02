cfd98b31b91e2dcddb63ca3e3414d67c24be638b
nicolasb <nicolas.benjamin.t@outlook.com>
2024-03-02 20:45:12 -0300

    Update Homework js arrays

--- C7 Arrays/ejercicios/06.js
+++ C7 Arrays/ejercicios/06.js
@@ -1,6 +1,12 @@
 function invertirArray(array) {
   // Invierte el arreglo array recibido por argumento.
   // Tu código:
+  var arrayInvertido = [];
+  for (var i = array.length -1 ; i >= 0 ; i--) {
+     arrayInvertido.push(array[i]);
+    
+  } 
+  return arrayInvertido;
 }
 
 module.exports = invertirArray;
