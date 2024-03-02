73e35889b33d2497f90b768bfc007561f8bdd138
nicolasb <nicolas.benjamin.t@outlook.com>
2024-03-02 00:30:43 -0300

    Update Homework arrays

--- C6 Bucles/ejercicios/24.js
+++ C6 Bucles/ejercicios/24.js
@@ -2,6 +2,12 @@ function invertirTexto(texto) {
    // La función recibe un argumento "texto" que es un string.
    // Tu objetivo es invertir el string y devolver el string invertido.
    // Tu código:
+
+var textoInvertido = "";
+   for (var i = texto.length -1; i >= 0; i--){
+      textoInvertido = textoInvertido + texto[i];
+   }
+  return textoInvertido;
 }
 
 module.exports = invertirTexto;
