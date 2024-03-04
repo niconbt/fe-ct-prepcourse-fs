bdd8629344bc2a3b1437bc39e54d80958891cede
nicolasb <nicolas.benjamin.t@outlook.com>
2024-03-04 14:31:01 -0300

    Update Homework

--- C7 Arrays/ejercicios/11.js
+++ C7 Arrays/ejercicios/11.js
@@ -2,6 +2,12 @@ function duplicarElementos(array) {
   // Duplica (multiplica x2) cada elemento del array de números.
   // Devuelve un array con los duplicados.
   // Tu código:
+  var resultado =[];
+  for (var i = 0; i < array.length; i++) {
+          resultado.push (array[i] * 2);
+    
+  }
+  return resultado;
 }
 
 module.exports = duplicarElementos;
