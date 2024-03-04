df5df020f85542c2d21915d2a96da5cb3eeec575
nicolasb <nicolas.benjamin.t@outlook.com>
2024-03-04 14:54:32 -0300

    Update Homework

--- C7 Arrays/ejercicios/10.js
+++ C7 Arrays/ejercicios/10.js
@@ -1,6 +1,14 @@
 function obtenerPrimerStringLargo(array) {
   // Devuelve el primer string con más de 5 caracteres en el array.
   // Tu código:
+  var cinco = "";
+
+for (var i = 0;  i < array.length; i++) 
+        if (typeof array[i] === 'string'){
+     cinco.push(array[i]);
+    }
+     return (cinco > 5); 
 }
+
 
 module.exports = obtenerPrimerStringLargo;
