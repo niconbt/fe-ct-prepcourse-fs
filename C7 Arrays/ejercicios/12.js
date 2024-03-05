cf3204b0d0f3e4e84fee8e9b46a4c45cf88efa9f
nicolasb <nicolas.benjamin.t@outlook.com>
2024-03-04 21:00:38 -0300

    Update Homework

--- C7 Arrays/ejercicios/12.js
+++ C7 Arrays/ejercicios/12.js
@@ -2,6 +2,13 @@ function convertirStringAMayusculas(array) {
   // Convierte a mayúsculas todos los strings del array.
   // Retorna el arreglo resultante.
   // Tu código:
+  var minusculas = [];
+  for (let i = 0; i < array.length; i++) {
+    if (typeof array[i] === 'string')
+    minusculas = toLowercase(array[i]);
+    
+  }
+  return minusculas;
 }
 
 module.exports = convertirStringAMayusculas;
