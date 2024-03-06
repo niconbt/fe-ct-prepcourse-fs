b501498b66a98f53c7627f9d1ab9c2c05f58c052
nicolasb <nicolas.benjamin.t@outlook.com>
2024-03-06 20:58:32 -0300

    Update Homework

--- C7 Arrays/ejercicios/15.js
+++ C7 Arrays/ejercicios/15.js
@@ -2,6 +2,19 @@ function encontrarIndiceMayor(array) {
   // Encuentra el índice del número más grande en el array de números.
   // Devuelve el valor de este índice.
   // Tu código:
+  var mayor = array[0];
+  var indice = 0;  
+  for (let i = 1; i < array.length; i++) {
+       if (array[i] > mayor){
+             mayor = array[i];
+             indice = i;
+       }
+  } 
+        
+    
+    return indice;
+         
+         
 }
 
 module.exports = encontrarIndiceMayor;
