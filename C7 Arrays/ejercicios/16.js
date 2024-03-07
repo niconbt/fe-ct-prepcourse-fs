e3515c2e4189cb74e65371e847d27304a2af9905
nicolasb <nicolas.benjamin.t@outlook.com>
2024-03-06 22:31:10 -0300

    Update Homework

--- C7 Arrays/ejercicios/16.js
+++ C7 Arrays/ejercicios/16.js
@@ -2,6 +2,13 @@ function multiplicarElementosPorIndice(array) {
   // Multiplica cada elemento del array por su índice.
   // Devuelve el nuevo arreglo con los resultados.
   // Tu código:
+
+var resultado = [];
+for (let i = 0; i < array.length; i++) {
+         
+         resultado.push(array[i] * i);
+      }
+        return resultado;
 }
 
 module.exports = multiplicarElementosPorIndice;
