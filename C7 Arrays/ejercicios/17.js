b65ce0d38b25e6e685b5343bfb004bafde40455a
nicolasb <nicolas.benjamin.t@outlook.com>
2024-03-06 22:46:11 -0300

    Update Homework

--- C7 Arrays/ejercicios/17.js
+++ C7 Arrays/ejercicios/17.js
@@ -1,6 +1,12 @@
 function agregarNumeros(arrayOfNums) {
   // Suma todos los elementos de arrayOfNums y retorna el resultado.
   // Tu código:
+  var suma = 0;
+  for (let i = 0; i < arrayOfNums.length; i++) {
+    suma += arrayOfNums[i];
+    
+  }
+     return suma;
 }
 
 module.exports = agregarNumeros;
