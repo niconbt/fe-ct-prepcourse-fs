f4e6cd1226d70b8b9214a6607159df6f1ecaf57c
nicolasb <nicolas.benjamin.t@outlook.com>
2024-03-06 22:58:53 -0300

    Update Homework

--- C7 Arrays/ejercicios/18.js
+++ C7 Arrays/ejercicios/18.js
@@ -1,6 +1,15 @@
 function promedioResultadosTest(resultadosTest) {
   // Itera sobre los elementos del arreglo resultadosTest y devuelve el promedio de las notas.
   // Tu código:
-}
+  var suma = 0;
+  var cantidad = 0;
+  for (let i = 0; i < resultadosTest.length; i++) {
+       suma += resultadosTest[i];
+       cantidad = suma / resultadosTest.length;
+    
+  }     
+    return cantidad;
+  }
+
 
 module.exports = promedioResultadosTest;
