037605785f4b3b112ccb0cca95191fbb257ded5e
nicolasb <nicolas.benjamin.t@outlook.com>
2024-03-13 17:45:23 -0300

    Update Homework

--- C8 Objects + Callbacks/callbacks/ejercicios/07.js
+++ C8 Objects + Callbacks/callbacks/ejercicios/07.js
@@ -2,6 +2,13 @@ function filter(arrayOfStrings) {
    // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
    // Luego retorna un nuevo arreglo con estos elementos.
    // Tu código:
+   var nuevo = [];
+   for (let i = 0; i < arrayOfStrings.length; i++) {
+      if(arrayOfStrings[i].toLowerCase().startsWith("a")){
+        nuevo.push(arrayOfStrings[i]);
+      }
+      
+   }return nuevo;
 }
 
 module.exports = filter;
