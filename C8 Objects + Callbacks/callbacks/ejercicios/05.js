f6580c02f03a0c5ca4378ae6ace7e14294a5c8e5
nicolasb <nicolas.benjamin.t@outlook.com>
2024-03-13 16:13:47 -0300

    Update Homework

--- C8 Objects + Callbacks/callbacks/ejercicios/05.js
+++ C8 Objects + Callbacks/callbacks/ejercicios/05.js
@@ -2,6 +2,11 @@ function forEach(array, cb) {
   // Recibes un arreglo y un callback.
   // Itera sobre el arreglo y por cada elemento iterado, ejecuta el callback con este valor.
   // Tu código:
+  for (let i = 0; i < array.length; i++) {
+     forEach(array[i]);
+    cb(array[i]);
+  }
+   
 }
 
 module.exports = forEach;
