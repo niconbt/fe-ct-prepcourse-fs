b732f5c37b9cd7c6f3c63c34b35775657d8c3ac7
nicolasb <nicolas.benjamin.t@outlook.com>
2024-03-13 16:35:09 -0300

    Update Homework

--- C8 Objects + Callbacks/callbacks/ejercicios/06.js
+++ C8 Objects + Callbacks/callbacks/ejercicios/06.js
@@ -3,6 +3,11 @@ function map(array, cb) {
   // Tiene que guardar el resultado devuelto por el callback en cada elemento dentro de un nuevo arreglo.
   // Retorna el nuevo arreglo.
   // Tu código:
+  var nuevo = [];
+  for (let i = 0; i < array.length; i++) {
+    nuevo.push(cb(array[i]));
+     } 
+  return nuevo;  
 }
 
 module.exports = map;
